f32def04b82b237c599f2a23ddf2cd8ead3b488d
Ashwani-Srivastava <ashwanisrivastava387@gmail.com>
2024-04-25 03:28:35 +0530

    fifth stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // Uncomment this block to pass the first stage
+
 use std::{
     io::{Read, Write},
     net::{TcpListener, TcpStream},
@@ -25,16 +26,28 @@ fn main() {
     }
 }
 fn handle_client(mut stream: TcpStream) {
-    let mut buf = [0; 7];
-    let mut buf2 = [0; 20];
-    buf.copy_from_slice(b"+PONG\r\n");
+    let mut buf2 = [0; 1024];
+
     loop {
         let read_bytes = stream.read(&mut buf2).expect("Failed to read");
         if (read_bytes == 0) {
             return;
         }
+        let s = String::from_utf8_lossy(&buf2[0..read_bytes]);
+
+        let ans = handle_request(&s.to_string());
+        let buf = ans.as_bytes();
         stream
-            .write_all(&buf[0..7])
+            .write_all(&buf[0..buf.len()])
             .expect("Failed to write to client");
+    }
+}
+fn handle_request(req: &String) -> String {
+    let echo_pattern = "echo\r\n";
+    if let Some(pos) = req.find(echo_pattern) {
+        let extracted_chars = &req[pos + echo_pattern.len()..];
+        extracted_chars.to_string()
+    } else {
+        "+PONG\r\n".to_string()
     }
 }
